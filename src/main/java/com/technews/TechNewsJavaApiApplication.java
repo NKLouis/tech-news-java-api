5f6c708256c0e855ddbdb2890f698827963fd54c
naritchaya louis <naritchayalouis@gmail.com>
2021-05-24 21:01:42 -0500

    built the entire structure of the database

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -2,6 +2,9 @@ package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "user")
 
 @SpringBootApplication
 public class TechNewsJavaApiApplication {
